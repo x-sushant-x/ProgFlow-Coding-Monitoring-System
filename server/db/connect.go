b4704de5168abe516972c8cde483b15e14c44a98
sushant102004 <sushant.dhiman9812@gmail.com>
2023-09-18 23:02:16 +0530

    Updated Readme

--- server/db/connect.go
+++ server/db/connect.go
@@ -1,16 +1,11 @@
 package db
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -21,21 +16,7 @@ const (
 	dbname   = "progflow"
 )
 
-func ConnectToDB() *mongo.Client {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("DATABASE_URI")))
-	if err != nil {
-		log.Fatal("Error connecting to database")
-	}
-
-	fmt.Println("Connected to database")
-
-	return client
-}
 
 func ConnectToPostgres() *sql.DB {
 	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
